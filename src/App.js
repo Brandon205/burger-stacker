c54fc81fa8b2baae5095e5a3c9e317c78bedc4b8
Brandon205 <brandonblack02@gmail.com>
2019-10-29 15:59:45 -0700

    Basic Layout made

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import BurgerList from './BurgerList';
+import BurgerStack from './BurgerStack';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    ingredients: [
+      {name: 'Kaiser Bun', color: 'saddlebrown'},
+      {name: 'Sesame Bun', color: 'sandybrown'},
+      {name: 'Gluten Free Bun', color: 'peru'},
+      {name: 'Lettuce Wrap', color: 'olivedrab'},
+      {name: 'Beef Patty', color: '#3F250B'},
+      {name: 'Soy Patty', color: '#3F250B'},
+      {name: 'Black Bean Patty', color: '#3F250B'},
+      {name: 'Chicken Patty', color: 'burlywood'},
+      {name: 'Lettuce', color: 'lawngreen'},
+      {name: 'Tomato', color: 'tomato'},
+      {name: 'Bacon', color: 'maroon'},
+      {name: 'Onion', color: 'lightyellow'}
+    ],
+    stack: []
+  }
+
+  render() {
+    return (
+      <div className='main-container'>
+        <BurgerList />
+        <BurgerStack />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/BurgerList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class BurgerList extends React.Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default BurgerList;
